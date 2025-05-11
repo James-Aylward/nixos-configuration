39a453e1fbe4a60b3801bc45ce3eab676dd7b1b7
James Aylward <james.michael.aylward@gmail.com>
2025-05-12 01:32:55 +1000

    adds nitch and fzf dependenies to shell

--- modules/shell.nix
+++ modules/shell.nix
@@ -3,8 +3,20 @@
   pkgs,
   ...
 }: {
+
+  home.packages = with pkgs; [
+    nitch
+  ];
+
+  programs.fzf = {
+    enable = true;
+    # TODO colors here
+  };
+
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
