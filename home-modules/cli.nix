35cdd2bff6051bb031382786fa6e99e5194b203a
James Aylward <james.michael.aylward@gmail.com>
2025-05-13 23:53:41 +1000

    adds file program

--- home-modules/cli.nix
+++ home-modules/cli.nix
@@ -1,20 +1,13 @@
 { config, pkgs, ... }: {
 
-  home.packages = with pkgs; [ tree unzip ncdu ];
+  home.packages = with pkgs; [ tree unzip ncdu file ];
+
 
   programs.vim.enable = true;
-
-  # TODO colors
   programs.yazi = { enable = true; };
-
-  # TODO colors
   programs.fzf = { enable = true; };
-
   programs.zoxide = { enable = true; };
-
   programs.lazygit.enable = true;
   home.shellAliases = { lg = "lazygit"; };
-
-  # TODO colors
   programs.btop = { enable = true; };
 }
