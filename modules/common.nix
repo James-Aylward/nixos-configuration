51682536ef0535a829696c08a0aa6fad99ced7db
James Aylward <james.michael.aylward@gmail.com>
2025-05-13 23:53:52 +1000

    adds font and image configuration

--- modules/common.nix
+++ modules/common.nix
@@ -4,6 +4,23 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   stylix.enable = true;
-  #stylix.targets.neovim.enable = false;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/espresso.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  stylix.image = ../images/nix.png;
+  stylix.polarity = "dark";
+  stylix.fonts = {
+    serif = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "DejaVu Serif";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans Mono";
+    };
+  };
 }
