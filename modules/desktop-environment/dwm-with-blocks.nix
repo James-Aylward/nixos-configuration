d0148b432220ada9db516bbe72b4c0712f410183
James Aylward <james.michael.aylward@gmail.com>
2025-05-14 08:48:13 +1000

    adds dwm with blocks

---
+++ modules/desktop-environment/dwm-with-blocks.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: {
+  imports = [ ./xkb.nix ];
+  services.xserver = {
+    enable = true;
+    windowManager.dwm = {
+      enable = true;
+      package = pkgs.dwm.overrideAttrs { src = ./dwm };
+    };
+  };
+}
+
