6eda2a76e63a4107362d00598639d640b2a93502
James Aylward <james.michael.aylward@gmail.com>
2025-05-15 00:56:03 +1000

    fixes highlightinlg

--- home-modules/neovim/nvim-treesitter.lua
+++ home-modules/neovim/nvim-treesitter.lua
@@ -1,4 +1,6 @@
 require("nvim-treesitter.configs").setup({
-	enable = true,
-				additional_vim_regex_highlighting = false,
+	highlight = {
+		enable = true,
+	},
+	additional_vim_regex_highlighting = false,
 })
