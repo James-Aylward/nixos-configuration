762e40702b49c27a45286ee6cfb5f1fbec266add
James Aylward <james.michael.aylward@gmail.com>
2025-05-14 11:37:17 +1000

    adds ncspot

--- home-modules/media.nix
+++ home-modules/media.nix
@@ -1,3 +1,14 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ vlc darktable imagemagick qimgv ];
+
+  programs.ncspot = {
+    enable = true;
+    settings = {
+      use_nerdfont = true;
+      notify = true;
+      gapless = true;
+      flip_status_indicators = true;
+    };
+  };
+
 }
