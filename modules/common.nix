02e9f7d6974ba4e401f18321e708c7b5ee1f917a
James Aylward <james.michael.aylward@gmail.com>
2025-05-14 17:36:16 +1000

    adds cursor

--- modules/common.nix
+++ modules/common.nix
@@ -10,12 +10,20 @@
   environment.systemPackages = with pkgs; [ xclip ];
 
   stylix.enable = true;
+  stylix.opacity.terminal = 0.85;
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   stylix.image = ../images/nix.png;
   stylix.targets.grub.useWallpaper = true;
   stylix.polarity = "dark";
-  stylix.fonts.sizes = { terminal = 8; };
+  
+  stylix.cursor = {
+        package = pkgs.rose-pine-cursor;
+        name = "BreezeX-RosePine-Linux";
+        size = 32;
+  };
+
+  stylix.fonts.sizes = { terminal = 10; };
   stylix.fonts = {
     serif = {
       package = pkgs.nerd-fonts.jetbrains-mono;
