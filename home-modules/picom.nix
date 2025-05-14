ab53b78bb4599c82465adc984c5db275266054e6
James Aylward <james.michael.aylward@gmail.com>
2025-05-15 00:05:56 +1000

    removes opacity

--- home-modules/picom.nix
+++ home-modules/picom.nix
@@ -1,25 +1,29 @@
 { config, pkgs, ... }: {
   services.picom = {
     enable = true;
-    fadeDelta = 5;
-    fade = true;
-    activeOpacity = 0.95;
-    inactiveOpacity = 0.95;
-    opacityRules = [
-      "100:class_g = 'firefox'"
-      "100:class_g = 'sioyek'"
-      "100:class_g = 'darktable'"
-      "100:class_g = 'feh'"
-      "100:class_g = 'obsidian'"
-      "100:class_g = 'org.remmina.Remmina'"
-      "100:class_g = 'code'"
-      "100:class_g = 'VirtualBox Machine'"
-      "100:class_g = 'qimgv'"
-      "100:class_g = 'vlc'"
-      "100:class_g = 'dmenu'"
-      "100:class_g = 'dwm'"
-      "100:class_g = 'qutebrowser'"
+    shadow = true;
+    shadowExclude = [
+    "class_g = 'dwm'"
+    "class_g = 'dmenu'"
+    "class_g = 'dwmsystray'"
     ];
-    fadeExclude = [ "class_g = 'dmenu'" ];
+    #activeOpacity = 1;
+    #inactiveOpacity = 0.98;
+    #opacityRules = [
+    #  "100:class_g = 'firefox'"
+    #  "100:class_g = 'sioyek'"
+    #  "100:class_g = 'darktable'"
+    #  "100:class_g = 'feh'"
+    #  "100:class_g = 'obsidian'"
+    #  "100:class_g = 'org.remmina.Remmina'"
+    #  "100:class_g = 'code'"
+    #  "100:class_g = 'VirtualBox Machine'"
+    #  "100:class_g = 'qimgv'"
+    #  "100:class_g = 'vlc'"
+    #  "100:class_g = 'dmenu'"
+    #  "100:class_g = 'dwm'"
+    #  "100:class_g = 'qutebrowser'"
+    #];
+    #fadeExclude = [ "class_g = 'dmenu'" ];
   };
 }
