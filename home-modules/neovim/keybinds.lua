ac7c8a3d06ed934e9589bfaaf1a49ba5342effab
James Aylward <james.michael.aylward@gmail.com>
2025-05-15 00:24:41 +1000

    adds keybinds and options

---
+++ home-modules/neovim/keybinds.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+-- Moving lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<M-j>", "o<Esc>")
+vim.keymap.set("n", "<M-k>", "O<Esc>")
+
+vim.keymap.set("n", "<leader>;", ":s/;$//<CR>")
+
+vim.keymap.set("n", "<leader>j", ":cnext<CR>")
+vim.keymap.set("n", "<leader>k", ":cprev<CR>")
+vim.keymap.set("n", "<leader>o", ":copen<CR>")
+vim.keymap.set("n", "<leader>c", ":cclose<CR>")
+
+vim.keymap.set("n", "<leader>m", ":make<CR>")
+vim.keymap.set("n", "<leader>s", ":ClangdSwitchSourceHeader<CR>")
+--vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- LSP stuff
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  end,
+})
---
+++ home-modules/neovim/options.lua
@@ -0,0 +1,45 @@
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tabs = 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.wrap = false
+
+-- Searching
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+
+-- Force latex
+vim.g.tex_flavor = "latex"
+
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevelstart = 99
+
+-- netrw
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+
+-- Autocommands
+-- Latex
+vim.api.nvim_create_autocmd( {"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.tex"},
+    command = [[ set wrap linebreak breakindent spell ]]
+})
