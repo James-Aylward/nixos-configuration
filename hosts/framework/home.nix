52a092f35a10b4490559e27dd0bed3a41428cc31
James Aylward <james.michael.aylward@gmail.com>
2025-06-13 16:12:37 +1000

    adds lutris

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -10,6 +10,7 @@
     ../../home-modules/cli.nix
     ../../home-modules/desktop-utility.nix
     ../../home-modules/redshift.nix
+    ../../home-modules/games.nix
     ../../home-modules/shell.nix
     ../../home-modules/terminal.nix
     ../../home-modules/version-control.nix
