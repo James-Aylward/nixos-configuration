dee39787c604c3520942c229fedddb0fd9e4d626
James Aylward <james.michael.aylward@gmail.com>
2025-10-16 15:22:58 +1100

    enables xwayland and ufwm

--- modules/desktop-environment/hyprland.nix
+++ modules/desktop-environment/hyprland.nix
@@ -10,5 +10,10 @@
   '';
 
   environment.systemPackages = with pkgs; [ cliphist ];
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+
+      enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+    };
 }
