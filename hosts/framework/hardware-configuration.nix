d893c78cc74057800568c08f07ed5e00b5e2c6eb
James Aylward <james.michael.aylward@gmail.com>
2025-10-23 14:41:01 +1000

    adds udev rule for corne keyboard

--- hosts/framework/hardware-configuration.nix
+++ hosts/framework/hardware-configuration.nix
@@ -37,5 +37,10 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+services.udev.extraRules = ''
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="100", TAG+="uaccess", TAG+="udev-acl"
+'';
+
 }
 
